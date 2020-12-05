74446e00a05f51ab0b5a3060aac1e101a0cfcde4
Rogerio Azevedo <rogerio.francis@gmail.com>
2020-12-05 11:46:23 -0400

    add gradient

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,16 +1,20 @@
 import React from 'react'
-import { Image, Text } from 'react-native'
+import { Image } from 'react-native'
 
 import logoIgm from '../../assets/logo.png'
+
+import Background from '../../components/Background'
 
 import * as Styles from './styles'
 
 const SignIn: React.FC = () => {
   return (
-    <Styles.Container>
-      <Styles.Title>Faça seu login</Styles.Title>
-      <Image source={logoIgm} />
-    </Styles.Container>
+    <Background>
+      <Styles.Container>
+        <Styles.Title>Faça seu login</Styles.Title>
+        <Image source={logoIgm} />
+      </Styles.Container>
+    </Background>
   )
 }
 
